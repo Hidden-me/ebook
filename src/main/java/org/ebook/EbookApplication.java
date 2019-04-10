fe8b0ce37aa443499e4b56cc87a3b801226a2e72
84461810 <431600414@qq.com>
2019-04-10 09:25:21 +0800

    20190410a: Database feature

--- src/main/java/org/ebook/EbookApplication.java
+++ src/main/java/org/ebook/EbookApplication.java
@@ -2,8 +2,9 @@ package org.ebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EbookApplication {
 
     public static void main(String[] args) {
